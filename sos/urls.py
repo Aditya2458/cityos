45d858c7a745e230dd3c6d35eaa1dc8824683cc3
Aditya Sahu <adityacbsa29@gmail.com>
2026-05-05 03:39:09 +0530

    Step 5: Alert assignment + resolve system added in urls

--- sos/urls.py
+++ sos/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import send_sos
+from .views import send_sos, get_alerts, accept_alert
 
 urlpatterns = [
     path('sos/', send_sos),
+     path('alerts/', get_alerts),
+    path('accept-alert/', accept_alert),
 ]
