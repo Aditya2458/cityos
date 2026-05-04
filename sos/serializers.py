2ed9e73865bc2c54002d0b1dd6a5378491971ed5
Aditya Sahu <adityacbsa29@gmail.com>
2026-05-05 03:37:29 +0530

    Step 5: Alert assignment + resolve system added

--- sos/serializers.py
+++ sos/serializers.py
@@ -4,3 +4,16 @@ from .models import SOSAlert, Location
 class SOSSerializer(serializers.Serializer):
     latitude = serializers.FloatField()
     longitude = serializers.FloatField()
+
+class LocationSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Location
+        fields = ['latitude', 'longitude', 'timestamp']
+
+
+class SOSAlertSerializer(serializers.ModelSerializer):
+    location = LocationSerializer()
+
+    class Meta:
+        model = SOSAlert
+        fields = ['id', 'user', 'status', 'created_at', 'location']
